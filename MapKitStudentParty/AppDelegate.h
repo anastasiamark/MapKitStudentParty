8580780ec6d38c839800fbc5f2a7afbe70b85ec1
Eugene Sokolenko <eugene@Eugenes-iMac.local>
2016-07-02 19:37:59 +0300

    Initial commit

---
+++ MapKitStudentParty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapKitStudentParty
+//
+//  Created by Eugene Sokolenko on 01.07.16.
+//  Copyright (c) 2016 Anastasia Markovskaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
